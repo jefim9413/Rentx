16b2eceeee777bf2dcc20975b891a9d694e44c7f
Fulano de Tal <jefferson.jsm3444@gmail.com>
2021-10-26 00:47:07 -0300

    atualização 26/10

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import fs from 'fs';
+import csvParse from 'csv-parse';
+
+class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse();
+    stream.pipe(parseFile);
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
