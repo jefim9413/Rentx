841021fb876cdbb01534a77091e4b33ae70a7766
Fulano de Tal <jefferson.jsm3444@gmail.com>
2021-11-10 17:37:36 -0300

    Atualização 10/11

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,16 +1,15 @@
+/* eslint-disable class-methods-use-this */
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-  private createCategoryUseCase: CreateCategoryUseCase;
-
-  constructor(createCategoryUseCase: CreateCategoryUseCase) {
-    this.createCategoryUseCase = createCategoryUseCase;
-  }
-
   async handle(request:Request, response:Response):Promise<Response> {
     const { name, description } = request.body;
-    await this.createCategoryUseCase.execute({ name, description });
+
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+
+    await createCategoryUseCase.execute({ name, description });
     return response.status(201).send();
   }
 }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import multer from 'multer';
-import createCategoryController from '../modules/cars/useCases/createCategory';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 
 const categoriesRoutes = Router();
 const upload = multer({
   dest: './tmp',
 });
-categoriesRoutes.post('/', (request, response) => createCategoryController().handle(request, response));
+const createCategoryController = new CreateCategoryController();
+categoriesRoutes.post('/', createCategoryController.handle);
 categoriesRoutes.get('/', (request, response) => listCategoriesController.handle(request, response));
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
 export { categoriesRoutes };
